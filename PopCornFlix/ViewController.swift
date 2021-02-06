7c83dca53ba1c743d1f108a4cfbe1bfa89af3472
Burak Nurçiçek <burak.nurcicek@consultant.turkcell.com.tr>
2021-02-06 21:14:30 +0300

    Initial commit

---
+++ PopCornFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PopCornFlix
+//
+//  Created by Burak Nurçiçek on 6.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
